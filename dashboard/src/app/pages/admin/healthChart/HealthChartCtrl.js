1fbbdf66b3eaf3508bbcea5acf4e779ece778ea1
shirishgoyal <shirish.goyal@gmail.com>
2017-04-25 17:09:23 -0700

    add rating and notes to health chart

--- dashboard/src/app/pages/admin/healthChart/HealthChartCtrl.js
+++ dashboard/src/app/pages/admin/healthChart/HealthChartCtrl.js
@@ -2,7 +2,7 @@
  * @author shirish.goyal
  * created on 22.12.2016
  */
-(function () {
+(function() {
     'use strict';
 
     angular.module('BlurAdmin.pages.admin')
@@ -13,19 +13,29 @@
         var layoutColors = baConfig.colors;
         var node = $element.children()[0];
 
-        var endDate = moment().toISOString();
-        var startDate = moment().subtract(1, 'month').toISOString();
+        var endDate = moment(); //.toISOString();
+        var startDate = moment().subtract(1, 'month'); //.toISOString();
 
-        $scope.$watch('microscopeData', function (value) {
+        $scope.$watch('microscopeData', function(value) {
             if (value != undefined && value.length > 0) {
                 var microscope = JSON.parse(value);
-                AdminMicroscope.health(microscope.id, startDate, endDate).then(function (response) {
+                AdminMicroscope.health(microscope.id, startDate, endDate).then(function(response) {
                     createHealthChart(node, response.data.results);
                 });
             }
-        }, true);
+        }, false);
 
         function createHealthChart(node, data) {
+
+            function legendHandler(evt) {
+                var id = evt.id;
+
+                for (var i1 in evt.chart.graphs) {
+                    if (id == evt.chart.graphs[i1].id) {
+                        evt.chart[evt.chart.graphs[i1].hidden ? "showGraph" : "hideGraph"](evt.chart.graphs[i1]);
+                    }
+                }
+            }
 
             var chart = AmCharts.makeChart(node, {
                 "type": "serial",
@@ -37,11 +47,13 @@
                     color: layoutColors.default,
                     axisColor: layoutColors.default,
                     gridColor: layoutColors.default,
-                    "id": "v1",
-                    "title": "Value",
-                    "position": "left",
-                    "autoGridCount": false,
-                    "labelFunction": function (value) {
+                    id: "v1",
+                    title: "Value",
+                    position: "left",
+                    minimum: 0,
+                    maximum: 5,
+                    autoGridCount: false,
+                    labelFunction: function(value) {
                         return "" + Math.round(value) + "";
                     }
                 }],
@@ -50,9 +62,12 @@
                     color: layoutColors.default,
                     "valueAxis": "v1",
                     "lineColor": layoutColors.danger,
-                    "fillColors": layoutColors.danger,
-                    "fillAlphas": 0.9,
+                    // "fillColors": layoutColors.danger,
+                    // "fillAlphas": 0.9,
                     "lineAlpha": 0.9,
+                    "bullet": "bubble",
+                    "bulletBorderAlpha": 1,
+                    "bulletSize": 0.01,
                     "type": "smoothedLine",
                     "title": "Activity",
                     "valueField": "activity",
@@ -66,9 +81,12 @@
                     "valueAxis": "v1",
                     color: layoutColors.default,
                     "lineColor": layoutColors.success,
-                    "fillColors": layoutColors.success,
-                    "fillAlphas": 0.7,
+                    // "fillColors": layoutColors.success,
+                    // "fillAlphas": 0.7,
                     "lineAlpha": 0.9,
+                    "bullet": "bubble",
+                    "bulletBorderAlpha": 1,
+                    "bulletSize": 0.01,
                     "type": "smoothedLine",
                     "title": "Population",
                     "valueField": "population",
@@ -82,9 +100,12 @@
                     "valueAxis": "v1",
                     color: layoutColors.default,
                     "lineColor": layoutColors.warning,
-                    "fillColors": layoutColors.warning,
-                    "fillAlphas": 0.5,
+                    // "fillColors": layoutColors.warning,
+                    // "fillAlphas": 0.5,
                     "lineAlpha": 0.9,
+                    "bullet": "bubble",
+                    "bulletBorderAlpha": 1,
+                    "bulletSize": 0.01,
                     "type": "smoothedLine",
                     "title": "Response",
                     "valueField": "response",
@@ -92,23 +113,37 @@
                     "lineColorField": layoutColors.warning,
                     "legendValueText": "[[value]]",
                     "balloonText": "[[title]]<br/><b style='font-size: 130%'>[[value]]</b>"
+                }, {
+                    "id": "g2",
+                    "valueAxis": "v1",
+                    color: layoutColors.default,
+                    "lineColor": layoutColors.info,
+                    // "fillColors": layoutColors.warning,
+                    // "fillAlphas": 0.5,
+                    "lineAlpha": 0.9,
+                    "bullet": "round",
+                    "bulletBorderAlpha": 1,
+                    "bulletSize": 0.01,
+                    "type": "smoothedLine",
+                    "title": "Rating",
+                    "valueField": "rating",
+                    "notesField": "notes",
+                    "columnWidth": 1,
+                    "lineColorField": layoutColors.info,
+                    "legendValueText": "[[value]]",
+                    "balloonText": "[[title]]: [[value]]<br/><p style='font-size: 90%'>[[notes]]</p>"
                 }],
                 "chartScrollbar": {
-                    "graph": "g1",
-                    "oppositeAxis": false,
-                    "offset": 30,
+                    oppositeAxis: false,
+                    offset: 30,
                     gridAlpha: 0,
                     color: layoutColors.default,
-                    scrollbarHeight: 50,
+                    scrollbarHeight: 30,
                     backgroundAlpha: 0,
                     selectedBackgroundAlpha: 0.05,
                     selectedBackgroundColor: layoutColors.default,
                     graphFillAlpha: 0,
                     autoGridCount: true,
-                    selectedGraphFillAlpha: 0,
-                    graphLineAlpha: 0.2,
-                    selectedGraphLineColor: layoutColors.default,
-                    selectedGraphLineAlpha: 1
                 },
                 "chartCursor": {
                     "pan": true,
@@ -129,10 +164,14 @@
                     "minorGridEnabled": true
                 },
                 "legend": {
-                    "useGraphSettings": false,
-                    "switchable": true,
-                    "position": "top",
-                    "color": layoutColors.default
+                    useGraphSettings: false,
+                    switchable: true,
+                    switchType: 'v',
+                    textClickEnabled: true,
+                    position: "top",
+                    color: layoutColors.default,
+                    "clickMarker": legendHandler,
+                    "clickLabel": legendHandler
                 },
                 "balloon": {
                     "borderThickness": 1,
