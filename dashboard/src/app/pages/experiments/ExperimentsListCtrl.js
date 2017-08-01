1d5cbe4e9248dc447edf656b436b4978f4a542d4
shirishgoyal <shirish.goyal@gmail.com>
2017-08-01 12:55:47 -0700

    load table once at start

--- dashboard/src/app/pages/experiments/ExperimentsListCtrl.js
+++ dashboard/src/app/pages/experiments/ExperimentsListCtrl.js
@@ -12,42 +12,44 @@
 		vm.pageSize    = 15;
 		vm.list        = [];
 		vm.displayed   = [];
-		vm.loading     = true;
+		vm.loading     = false;
 
 		vm.filter = function (tablestate) {
 			vm.getList(tablestate);
 		};
 
 		vm.getList = function (tablestate) {
-			vm.loading = true;
+			if (!vm.loading) {
+				vm.loading = true;
 
-			var page = Math.floor(tablestate.pagination.start / tablestate.pagination.number);
+				var page = Math.floor(tablestate.pagination.start / tablestate.pagination.number);
 
-			Experiment.list(page + 1, vm.pageSize, '').then(function (res) {
+				Experiment.list(page + 1, vm.pageSize, '').then(function (res) {
 
-				vm.experiments = lodash.map(res.data.results, function (experiment) {
-					experiment.status = Experiment.STATUS_DISPLAY[experiment.status];
-					return experiment;
+					vm.experiments = lodash.map(res.data.results, function (experiment) {
+						experiment.status = Experiment.STATUS_DISPLAY[experiment.status];
+						return experiment;
+					});
+
+					vm.list.concat(vm.experiments);
+					vm.displayed = [].concat(vm.experiments);
+
+					vm.total = res.data.items.total;
+					vm.pages = res.data.pages.total;
+
+					tablestate.pagination.start          = page * vm.pageSize;
+					tablestate.pagination.numberOfPages  = vm.pages;
+					tablestate.pagination.totalItemCount = vm.total;
+					tablestate.pagination.current        = page;
+
+					vm.currentPage = page + 1;
+
+				}).finally(function () {
+					$timeout(function () {
+						vm.loading = false;
+					}, 1000);
 				});
-
-				vm.list.concat(vm.experiments);
-				vm.displayed = [].concat(vm.experiments);
-
-				vm.total = res.data.items.total;
-				vm.pages = res.data.pages.total;
-
-				tablestate.pagination.start          = page * vm.pageSize;
-				tablestate.pagination.numberOfPages  = vm.pages;
-				tablestate.pagination.totalItemCount = vm.total;
-				tablestate.pagination.current        = page;
-
-				vm.currentPage = page + 1;
-
-			}).finally(function () {
-				$timeout(function () {
-					vm.loading = false;
-				}, 1000);
-			});
+			}
 		};
 
 		vm.download = function (id) {
