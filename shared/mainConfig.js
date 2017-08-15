a2f3cb79ac2838f8e1781a503b78df6d9daa34cf
githubchik <tahrina007@yahoo.com>
2017-08-14 17:26:10 -0700

    Adding eug-5

--- shared/mainConfig.js
+++ shared/mainConfig.js
@@ -259,6 +259,30 @@ var exports = module.exports = {
       allowedPorts: [20015, 20016, 20017, 20018, 20019],
     },
   }, {
+    index: 5,
+    name: 'eug5',
+    description: 'batch 1 bpu mics, 4x, 4 leds, static thor labs bg light',
+    type: 'batch 1 bpu mics',
+    magnification: 4,
+    backgroundLighting: 'white',
+    stimulusType: '4leds',
+    stimulusDescription: '4 leds controls by arduino',
+    allowedGroups: ['admin', 'default', 'scripter', 'nwg', 'golabz'],
+    isOn: false,
+    isArduino: false,
+    maxLightIntentisy: 1, //percent of lightValues used on leds.js
+    localAddr: {
+      ip: '192.168.1.205',
+      serverPort: 8090,
+      webcamPort: 8080,
+    },
+    publicAddr: {
+      ip: g_publicIP,
+      serverPort: 20026,
+      webcamPort: 20025,
+      allowedPorts: [20015, 20016, 20017, 20018, 20019],
+    },
+  }, {
     index: 15,
     name: 'eug15',
     description: 'batch 2.0 bpu mics, 4x, 4 leds, static led diffuser',
