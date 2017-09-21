5cfcbdf0bf1782a33f20bf5d0f19852580fd1d5a
shirishgoyal <shirish.goyal@gmail.com>
2017-09-21 11:40:18 -0700

    projector switch

--- bpu/libs/boardConfig.js
+++ bpu/libs/boardConfig.js
@@ -1,20 +1,24 @@
+var os = require('os');
+var env = require('dotenv');
+env.config();
+
 var MODE = {
-	ANALOG:  0,
+	ANALOG: 0,
 	DIGITAL: 1,
 	SOFTPWM: 2,
 	HARDPWM: 3,
-	SOCKET:  4
+	SOCKET: 4
 };
 
 var IO = {
-	INPUT:           0,
-	OUTPUT:          1,
-	PWM_OUTPUT:      2,
+	INPUT: 0,
+	OUTPUT: 1,
+	PWM_OUTPUT: 2,
 	SOFT_PWM_OUTPUT: 4
 };
 
 var TYPE = {
-	STATE:   0,
+	STATE: 0,
 	NUMERIC: 1
 };
 
@@ -25,95 +29,103 @@ var BoardConfig = {
 	RunPin: 9,
 
 	version: '1.0',
-	model:   'raspberrypi 3.0',
+	model: 'raspberrypi 3.0',
 	devices: [{
-		name:    'topLED',
-		pin:     24,
-		mode:    MODE.SOFTPWM,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'topLED',
+		isEnabled: true,
+		pin: 24,
+		mode: MODE.SOFTPWM,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     100,
+			min: 0,
+			max: 100,
 			default: 0
 		}
 	}, {
-		name:    'rightLED',
-		pin:     25,
-		mode:    MODE.SOFTPWM,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'rightLED',
+		isEnabled: true,
+		pin: 25,
+		mode: MODE.SOFTPWM,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     100,
+			min: 0,
+			max: 100,
 			default: 0
 		}
 	}, {
-		name:    'bottomLED',
-		pin:     3,
-		mode:    MODE.SOFTPWM,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'bottomLED',
+		isEnabled: true,
+		pin: 3,
+		mode: MODE.SOFTPWM,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     100,
+			min: 0,
+			max: 100,
 			default: 0
 		}
 	}, {
-		name:    'leftLED',
-		pin:     8,
-		mode:    MODE.SOFTPWM,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'leftLED',
+		isEnabled: true,
+		pin: 8,
+		mode: MODE.SOFTPWM,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     100,
+			min: 0,
+			max: 100,
 			default: 0
 		}
 	}, {
-		name:    'diffuser',
-		pin:     10,
-		mode:    MODE.DIGITAL,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'diffuser',
+		isEnabled: true,
+		pin: 10,
+		mode: MODE.DIGITAL,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     1,
+			min: 0,
+			max: 1,
 			default: 1
 		}
 	}, {
-		name:    'valve',
-		pin:     7,
-		mode:    MODE.DIGITAL,
-		type:    TYPE.NUMERIC,
-		io:      IO.OUTPUT,
+		name: 'valve',
+		isEnabled: true,
+		pin: 7,
+		mode: MODE.DIGITAL,
+		type: TYPE.NUMERIC,
+		io: IO.OUTPUT,
 		options: {
-			min:     0,
-			max:     1,
+			min: 0,
+			max: 1,
 			default: 0
 		}
 	}, {
-		name:    'camera',
-		pin:     32000,
-		mode:    MODE.SOCKET,
-		type:    TYPE.STATE,
-		io:      IO.OUTPUT,
+		name: 'camera',
+		isEnabled: (parseInt(process.env.IS_CAMERA_ENABLED || '1') == 1),
+		pin: 32000,
+		mode: MODE.SOCKET,
+		type: TYPE.STATE,
+		io: IO.OUTPUT,
 		options: {
-			states:  {
-				ON:  'start',
+			states: {
+				ON: 'start',
 				OFF: 'stop'
 			},
 			default: 'stop'
 		}
 	}, {
-		name:    'projector',
-		pin:     32001,
-		mode:    MODE.SOCKET,
-		type:    TYPE.STATE,
-		io:      IO.OUTPUT,
+		name: 'projector',
+		isEnabled: (parseInt(process.env.IS_PROJECTOR_ENABLED || '1') == 1),
+		pin: 32001,
+		mode: MODE.SOCKET,
+		type: TYPE.STATE,
+		io: IO.OUTPUT,
 		options: {
-			states:  {
-				ON:  'start',
+			states: {
+				ON: 'start',
 				OFF: 'stop'
 			},
 			default: 'stop'
@@ -121,7 +133,7 @@ var BoardConfig = {
 	}]
 };
 
-module.exports.TYPE        = TYPE;
-module.exports.IO          = IO;
-module.exports.MODE        = MODE;
+module.exports.TYPE = TYPE;
+module.exports.IO = IO;
+module.exports.MODE = MODE;
 module.exports.BoardConfig = BoardConfig;
