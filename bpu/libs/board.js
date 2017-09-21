8044378078afbfb64e1d758193539f942d6b2f14
shirishgoyal <shirish.goyal@gmail.com>
2017-09-21 11:42:24 -0700

    device enable switch

--- bpu/libs/board.js
+++ bpu/libs/board.js
@@ -1,32 +1,34 @@
 var lodash = require('lodash');
 
-var logger      = require('./logging');
-var constants   = require('./constants');
+var logger = require('./logging');
+var constants = require('./constants');
 
 var boardConfig = require('./boardConfig').BoardConfig;
-var Device      = require('./device');
+var Device = require('./device');
 
 function Board() {
 	this.devices = {};
 }
 
-Board.prototype.configure = function () {
+Board.prototype.configure = function() {
 	var that = this;
 
 	// todo make it enable disable device based on config
 	// todo control from .env file for what devices can work
 	// todo control from controller so that we can do this from frontend
-	lodash.each(boardConfig.devices, function (device) {
-		that.devices[device.name] = new Device(device);
-		that.devices[device.name].configure();
+	lodash.each(boardConfig.devices, function(device) {
+		if (device.isEnabled) {
+			that.devices[device.name] = new Device(device);
+			that.devices[device.name].configure();
+		}
 	});
 };
 
-Board.prototype.getState = function () {
+Board.prototype.getState = function() {
 	return this.devices;
 };
 
-Board.prototype.setDevice = function (deviceName, value) {
+Board.prototype.setDevice = function(deviceName, value) {
 	if (deviceName in this.devices) {
 		var device = this.devices[deviceName];
 
@@ -36,7 +38,7 @@ Board.prototype.setDevice = function (deviceName, value) {
 	}
 };
 
-Board.prototype.getDevice = function (deviceName) {
+Board.prototype.getDevice = function(deviceName) {
 	if (deviceName in this.devices) {
 		return this.devices[deviceName];
 	}
@@ -44,35 +46,35 @@ Board.prototype.getDevice = function (deviceName) {
 	return null;
 };
 
-Board.prototype.startRecording = function () {
+Board.prototype.startRecording = function() {
 	this.setDevice('camera', 'start');
 };
 
-Board.prototype.stopRecording = function () {
+Board.prototype.stopRecording = function() {
 	this.setDevice('camera', 'stop');
 };
 
-Board.prototype.startProjector = function () {
+Board.prototype.startProjector = function() {
 	this.setDevice('projector', 'start');
 };
 
-Board.prototype.stopProjector = function () {
+Board.prototype.stopProjector = function() {
 	this.setDevice('projector', 'stop');
 };
 
-Board.prototype.flush = function (duration) {
+Board.prototype.flush = function(duration) {
 	var that = this;
 
 	// don't flush more than 20 msec
 	// better utilize sample
 	// todo make it better so that it achieves goal for flushing => more or less euglena, clean sample etc.
 	if (duration > 20) {
-		duration = 20;  //msec
+		duration = 20; //msec
 	}
 
 	this.setDevice('valve', 1);
 
-	setTimeout(function () {
+	setTimeout(function() {
 		that.setDevice('valve', 0);
 	}, duration);
 };
